f428a56654b42eb1c3e31af23dba8690f1a4414e
Prasoon Kumar <prasoonkumar467@gmail.com>
2025-02-22 11:52:37 +0530

    db connected

---
+++ server/app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+import connectDB from "./config/connectDB.js";
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+connectDB()
+  .then(() => {
+    console.log("Database connected");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Error connecting to database :", err);
+  });
