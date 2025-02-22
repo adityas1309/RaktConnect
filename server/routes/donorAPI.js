fb79414844e0233e16ca42867304dfa58a5ca07e
Prasoon Kumar <prasoonkumar467@gmail.com>
2025-02-23 02:02:00 +0530

    .

--- server/routes/donorAPI.js
+++ server/routes/donorAPI.js
@@ -3,13 +3,13 @@ import Donor from "../models/donor.js";
 import jwt from "jsonwebtoken"
 const JWT_SECRET = process.env.JWT_SECRET;
 
-donorRouter = Router();
+const donorRouter = Router();
 
 const verifyToken = (req, res, next) => {
   const { token } = req.body;
   if (!token) return res.status(401).json({ message: "Access Denied" });
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+  jwt.verify(token, JWT_SECRET, (err, decoded) => {
     if (err) return res.status(403).json({ message: "Invalid Token" });
     req.email = decoded.email;
     next();
