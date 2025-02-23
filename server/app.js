e62b745bb7d3f7e672aaabac0e2e550920b11bb6
Rajiv <lucky81205@gmail.com>
2025-02-23 12:59:36 +0530

    Deploy Fix

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin:[ "http://localhost:5173" , "https://raktconnect.onrender.com" ],
     credentials: true,
   })
 );
