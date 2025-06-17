64ed16d056c679e65fb1b3e4f8b855bfba917cac
ayush <ayushjhasahab07@gmail.com>
2025-06-17 14:05:09 +0530

    Fix: Donor auth issue and improved toast design

--- client/src/apiConfig.js
+++ client/src/apiConfig.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000"; // local backend
+// const BASE_URL = "http://localhost:5000"; // local backend
 // OR
-// const BASE_URL = "https://rakt-connect-server.vercel.app"; // production backend
+const BASE_URL = "https://rakt-connect-server.vercel.app"; // production backend
 export default BASE_URL;
