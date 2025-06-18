844a7eb51cd0375f8242533d447161f6a3a7190f
ayush <ayushjhasahab07@gmail.com>
2025-06-18 09:47:14 +0530

    Minor fixes:change the url of apiconfig.js

--- client/src/apiConfig.js
+++ client/src/apiConfig.js
@@ -1,4 +1,3 @@
-const BASE_URL = "http://localhost:3000"; // local backend
-
-// const BASE_URL = "https://rakt-connect-server.vercel.app"; // production backend
+// const BASE_URL = "http://localhost:5000"; // local backend
+const BASE_URL = "https://rakt-connect-server.vercel.app"; // production backend
 export default BASE_URL;
